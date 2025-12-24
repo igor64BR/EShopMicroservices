2984fa9e7ede1b25ddc628b250083b38f3bfad29
Baiocco <igor64br@gmail.com>
2025-12-24 00:24:06 -0300

    feat: Implementation of Catalog.API CreateProduct handler partially, and CQRS custom interfaces

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/Handler.cs
@@ -0,0 +1,33 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record Command(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price) : ICommand<Result>;
+
+public record Result(Guid Id);
+
+public class Handler : ICommandHandler<Command, Result>
+{
+    public async Task<Result> Handle(Command command, CancellationToken cancellationToken)
+    {
+        var product = new Product()
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // TODO: Save product to database
+        var resultId = Guid.NewGuid(); // Simulate database generated Id
+
+        return new(resultId);
+    }
+}
