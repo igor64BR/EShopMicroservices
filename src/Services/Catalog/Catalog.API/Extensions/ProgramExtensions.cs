d0d28782eeb659556cccb21ca246cf97e36f6b8a
Baiocco <igor64br@gmail.com>
2025-12-28 20:39:37 -0300

    refact: add global behaviours and DI configurations in Program.cs

---
+++ src/Services/Catalog/Catalog.API/Extensions/ProgramExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Catalog.API.Extensions;
+
+public static class ProgramExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public void ConfigureSwagger()
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+    }
+
+    extension(WebApplication app)
+    {
+        public void ConfigureSwagger()
+        {
+            if (!app.Environment.IsDevelopment())
+                return;
+
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.DocumentTitle = "Catalog API Explorer";
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+            });
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,25 @@
+using Catalog.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services here
+builder.Services.AddCarter();
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+builder.Services.AddMarten(cfg =>
+{
+    cfg.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+builder.Services.ConfigureSwagger();
 
 var app = builder.Build();
 
 // Configure the HTTP requestpipeline here
+app.ConfigureSwagger();
+app.MapCarter();
 
 app.Run();
