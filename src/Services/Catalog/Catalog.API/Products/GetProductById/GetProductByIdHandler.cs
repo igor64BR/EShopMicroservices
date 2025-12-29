cf64d1559be32e35e80c631881c32bace78f546f
Baiocco <igor64br@gmail.com>
2025-12-28 23:10:34 -0300

    feat: Endpoint for GetProductById

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+public class GetProductByIdHandler(
+    IDocumentSession session,
+    ILogger<GetProductByIdHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Handling GetProductById Query for Id: {Id}", request.Id);
+
+        var product = await session.LoadAsync<Product>(request.Id, cancellationToken);
+
+        if (product == null)
+        {
+            logger.LogWarning("Product with Id: {Id} not found", request.Id);
+            throw new ProductNotFoundException();
+        }
+
+        return new(product);
+    }
+}
