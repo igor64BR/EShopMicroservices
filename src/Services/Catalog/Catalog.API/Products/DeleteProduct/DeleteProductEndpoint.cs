91d87aadc44c80489f3b6ab55cf92fa4b22d1124
Baiocco <igor64br@gmail.com>
2025-12-29 00:18:41 -0300

    feat: DeleteProduct Endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductRequest(Guid Id);
+
+public class DeleteProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products", async (
+            [FromBody] DeleteProductRequest request,
+            ISender sender,
+            CancellationToken cancellationToken) =>
+        {
+            var command = request.Adapt<DeleteProductCommand>();
+
+            await sender.Send(command, cancellationToken);
+
+            return Results.NoContent();
+        }).WithName("DeleteProduct")
+        .WithSummary("Delete a Product")
+        .WithDescription("Delete a Product based on the specified id")
+        .Produces(StatusCodes.Status204NoContent)
+        .Produces(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand;
+
+public class DeleteProductHandler(
+    IDocumentSession session,
+    ILogger<DeleteProductHandler> logger) : ICommandHandler<DeleteProductCommand>
+{
+    public async Task<Unit> Handle(DeleteProductCommand request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Deleting product with ID {ProductId}", request.Id);
+
+        var product = await session.LoadAsync<Product>(request.Id, cancellationToken);
+
+        if (product == null)
+        {
+            logger.LogError("Product with ID {ProductId} not found", request.Id);
+            throw new ProductNotFoundException();
+        }
+
+        session.Delete(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new();
+    }
+}
