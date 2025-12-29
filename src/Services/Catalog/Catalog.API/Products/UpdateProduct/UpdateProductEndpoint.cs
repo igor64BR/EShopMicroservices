40f62d89e765e234b74532e5a838e8e1b7673f00
Baiocco <igor64br@gmail.com>
2025-12-29 00:05:30 -0300

    feat: UpdateProduct Endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductEndpointRequest(
+    Guid Id,
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price);
+
+public class UpdateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (
+            UpdateProductEndpointRequest request,
+            ISender sender,
+            CancellationToken cancellationToken) =>
+        {
+            var command = request.Adapt<UpdateProductCommand>();
+
+            await sender.Send(command, cancellationToken);
+
+            return Results.NoContent();
+        }).WithName("UpdateProduct")
+        .WithSummary("Update a Product")
+        .WithDescription("Updates a product given the specific id in request body")
+        .Produces(StatusCodes.Status204NoContent)
+        .Produces(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status400BadRequest);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductCommand(
+    Guid Id,
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price) : ICommand;
+
+public class UpdateProductHandler(
+    IDocumentSession session,
+    ILogger<UpdateProductHandler> logger) : ICommandHandler<UpdateProductCommand>
+{
+    async Task<Unit> IRequestHandler<UpdateProductCommand, Unit>.Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Updating product with ID {ProductId}", request.Id);
+
+        var product = await session.LoadAsync<Product>(request.Id, cancellationToken);
+
+        if (product == null)
+        {
+            logger.LogError("Product with ID {ProductId} not found", request.Id);
+            throw new ProductNotFoundException();
+        }
+
+        request.Adapt(product);
+
+        session.Update(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new();
+    }
+}
